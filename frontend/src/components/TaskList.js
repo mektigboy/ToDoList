fac1303119eda0b4e36c3503a38b018e44b3ddaa
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-04 01:12:02 -0500

    Update & make files prittier

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,43 +1,65 @@
-//import React from 'react';
+const formatDate = (rawDate) => {
+  if (!rawDate) {
+    return "";
+  }
+  const _date = new Date(parseInt(rawDate));
+  const date = `${_date.getDate() + 1}/${
+    _date.getMonth() + 1
+  }/${_date.getFullYear()}`;
+  const time = `${_date.getHours()}h ${_date.getMinutes()}m`;
+  return `${date} - ${time}`;
+};
 
-function NewTask({ createTask }) {
-  const onSubmit = (e) => {
-    e.preventDefault();
-    createTask(e.target.elements[0].value, e.target.elements[1].value);
-  };
+function TaskList({ tasks, toggleDone }) {
+  const renderTask = (task) => (
+    <tr key={task.id}>
+      <td>{task.id.toString()}</td>
+      <td>{formatDate(task.date.toString())}</td>
+      <td>{task.content}</td>
+      <td>{task.author}</td>
+      <td>
+        <input
+          type="checkbox"
+          onChange={() => toggleDone(task.id.toString())}
+          checked={!!task.done}
+        />
+      </td>
+      <td>
+        {task.dateComplete.toString() !== "0"
+          ? formatDate(task.dateComplete.toString())
+          : ""}
+      </td>
+    </tr>
+  );
 
   return (
     <div className="card">
       <div className="row">
         <div className="col-sm-12">
-          <h2 className="orange">Create Task</h2>
+          <h2 className="orange">Tasks</h2>
         </div>
       </div>
       <div className="row">
-        <form id="new-task" className="col-sm-12" onSubmit={(e) => onSubmit(e)}>
-          <div className="form-group">
-            <label htmlFor="task-content">Content</label>
-            <input
-              id="task-content"
-              type="text"
-              className="form-control"
-            ></input>
-          </div>
-          <div className="form-group">
-            <label htmlFor="task-author">Author</label>
-            <input
-              id="task-author"
-              type="text"
-              className="form-control"
-            ></input>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        <div className="col-sm-12">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Content</th>
+                <th>Author</th>
+                <th>Done</th>
+                <th>Date Complete</th>
+              </tr>
+            </thead>
+            <tbody id="tasks">
+              {tasks && tasks.map((task) => renderTask(task))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
 }
 
-export default NewTask;
+export default TaskList;

192d87a8f261bbad684532ada00367c10c156bdb
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-03 23:18:27 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.14",
+  paths: {
+      artifacts: "frontend/src/artifacts"
+  }
 };
